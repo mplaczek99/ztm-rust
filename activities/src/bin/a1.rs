21256cc3a312a8294a7885f9dd244a0036ad1056
mplaczek99 <mplaczek99@gmail.com>
2026-05-10 22:08:49 -0500

    Finished Activity 1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,6 +8,21 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn print_first() {
+    println!("Michael");
+}
+
+fn print_last() {
+    println!("Placzek");
+}
+
+fn main() {
+    // * Use a function to display your first name
+    print_first();
+    // * Use a function to display your last name
+    print_last();
+    // * Use the println macro to display messages to the terminal
+    println!("Message to terminal");
+}
 
 
