9ebe3140ad44a07b2a2e1add1800b42da342574f
mplaczek99 <mplaczek99@gmail.com>
2026-05-12 20:30:59 -0500

    Completed a2 lesson

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,6 +8,20 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// * Use a function to add two numbers together
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// * Use a function to display the result
+fn print_result(num: i32) {
+    // * Use the "{:?}" token in the println macro to display the result
+    println!("Result: {:?}", num);
+}
+
+fn main() {
+    let result = add(5, 5);
+    print_result(result);
+}
 
 
