146fbe7a7d681b44d438ef77ee29e6fe887ea5f7
mplaczek99 <mplaczek99@gmail.com>
2026-05-17 20:57:36 -0500

    Finished lesson on expressions

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -12,5 +12,21 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+// * Use a function to print the messages
+fn print_is_greater(is_gt_100: bool) {
+    // * Use a match expression to determine which message
+    //   to print
+    match is_gt_100 {
+        true => println!("it's big"),
+        false => println!("it's small"),
+    }
+}
 
+fn main() {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    //   is > 100 or <= 100
+    let num = 999;
+    let is_gt_100 = num > 100;
+    print_is_greater(is_gt_100);
+}
