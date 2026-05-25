4fb8de4779fcdeb25698973da06539b591d4f7ff
mplaczek99 <mplaczek99@gmail.com>
2026-05-25 00:10:20 -0500

    Finished Activity using Documentation

--- activities/src/bin/a17.rs
+++ activities/src/bin/a17.rs
@@ -12,6 +12,16 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+    // * Utilize standard library functionality to
+    //   transform the string to lowercase and uppercase
+    // * Use 'rustup doc' in a terminal to open the standard library docs
+    // * Navigate to the API documentation section
+    // * Search for functionality to transform a string (or str)
+    //   to uppercase and lowercase
+    //   * Try searching for: to_uppercase, to_lowercase
+    let name = "Michael";
 
-
+    println!("Uppercase: {:?}", name.to_uppercase());
+    println!("Lowercase: {:?}", name.to_lowercase());
+}
