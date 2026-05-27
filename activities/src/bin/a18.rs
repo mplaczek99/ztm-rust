e686e647a1802e8e7a1a7b6727ef341b31cac467
mplaczek99 <mplaczek99@gmail.com>
2026-05-26 23:19:23 -0500

    Finished activity on Result

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -16,6 +16,41 @@
 //   * For the Ok variant, print any message you want
 //   * For the Err variant, print out the error message
 
-fn main() {}
+//   * Implement Debug print functionality using `derive`
+#[derive(Debug)]
+// * Create an structure named `Adult` that represents a person aged 21 or older:
+struct Adult {
+    //   * The structure must contain the person's name and age
+    name: String,
+    age: u8,
+}
 
+// * Implement a `new` function for the `Adult` structure that returns a Result:
+impl Adult {
+    fn new(name: &str, age: u8) -> Result<Self, &str> {
+        if age >= 21 {
+            Ok(Self {
+                age,
+                name: name.to_owned(),
+            })
+        } else {
+            Err("Age must be 21 or over")
+        }
+    }
+}
 
+fn print_information(result: &Result<Adult, &str>) {
+    match result {
+        Ok(a) => println!("Created adult: {:?} {:?}", a.name, a.age),
+        Err(e) => println!("Could not create adult: {:?}", e),
+    }
+}
+
+fn main() {
+    // * Instantiate two `Adult` structures:
+    let adult_1 = Adult::new("Sebastian", 19);
+    let adult_2 = Adult::new("Michael", 26);
+
+    print_information(&adult_1);
+    print_information(&adult_2);
+}
