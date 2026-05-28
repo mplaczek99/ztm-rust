370dc4ca5e909cbd0e38a3f625e05dfea939f84b
mplaczek99 <mplaczek99@gmail.com>
2026-05-27 21:03:46 -0500

    Completed activity on HashMaps

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,6 +13,24 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
 
+fn main() {
+    let mut store = HashMap::new();
+    store.insert("Chairs".to_owned(), 5);
+    store.insert("Beds".to_owned(), 3);
+    store.insert("Tables".to_owned(), 2);
+    store.insert("Couches".to_owned(), 0);
 
+    let mut total_number = 0;
+    for (item, &number) in store.iter() {
+        total_number = total_number + number;
+        let count = if number == 0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{:?}", number)
+        };
+        println!("Item: {:?}, Count: {:?}", item, count);
+    }
+    println!("{:?} items", total_number);
+}
