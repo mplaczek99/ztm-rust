bae049752810f079844dab588a0894c4d9980ab9
mplaczek99 <mplaczek99@gmail.com>
2026-05-31 16:00:58 -0500

    Finish activity on test

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -23,14 +23,75 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
+    if b == 0 {
+        return None;
+    }
     Some(a / b)
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
 
+// * Write tests for the existing program to ensure proper functionality.
+#[cfg(test)]
+mod test {
+    use crate::*;
 
+    // * Create at least two test cases for each function.
+    #[test]
+    fn clamp_returns_number_when_inside_range() {
+        let result = clamp(5, 1, 10);
+        let expected = 5;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn clamp_returns_lower_when_number_is_too_low() {
+        let result = clamp(-5, 1, 10);
+        let expected = 1;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn clamp_returns_upper_when_number_is_too_high() {
+        let result = clamp(50, 1, 10);
+        let expected = 10;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn div_returns_some_when_division_is_valid() {
+        let result = div(10, 2);
+        let expected = Some(5);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn div_returns_none_when_dividing_by_zero() {
+        let result = div(10, 0);
+        let expected = None;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn concat_combines_two_words_without_space() {
+        let result = concat("hello", "world");
+        let expected = String::from("helloworld");
+
+        assert_eq!(result, expected);
+    }
+
+    // * Use `cargo test` to test the program.
+    // * There are intentional bugs in the program that need to be fixed.
+    //   * Check the documentation comments for the functions to
+    //     determine how the they should operate.
+}
